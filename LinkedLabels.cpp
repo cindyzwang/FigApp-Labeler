33086cbd2e04603ebd744cbbf711e5623196f08d
czwang113 <czwang@bu.edu>
2016-04-27 22:02:46 -0400

    Added files via upload
    old way: using paths would mess up the output file
    new way: reformatted output file name so you can use paths

--- LinkedLabels.cpp
+++ LinkedLabels.cpp
@@ -1,10 +1,36 @@
+/*
+"old label" refers to a label in the input file
+"new label" refers to a label in the output file
+
+Problem:
+	for large, collaborative reports, relabeling figures and appendices 
+	in the correct order takes so much more time than it's worth 
+	but you have to do it
+
+How to use:
+	users label figures/appendices with any integer (-2^16 < label < 2^16-1)
+	set input file
+	this program:
+		1) reads and copies file
+		2) as it reads, it looks for labels
+		3) renumbers in order for the output file
+			a) if it is referenceing a new fig/app, stores the old label
+			b) if it is referencing a previous fig/app, finds that old label, 
+			   returns the new label (index) used for that fig/app
+
+*/
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cstring>
 #include <algorithm>
 using namespace std;
 
-void readFile();
+void changeFiles(string path_in);
 bool isFig(const string& str, ifstream& infile);
 bool isApp(const string& str, ifstream& infile);
 void compareStrings();
@@ -13,11 +39,18 @@ bool csCompareChar(char a, char b);
 bool compareStr(const string& str1, const string& str2);
 void writeWhiteSpaces(ifstream& ifile, ofstream& ofile);
 int searchNStoreOldLabels(int oldLabels[], int oldLabel, int& numLabels);
+string newFileName(string path_in);
+//*void convertToLetters();
 
-const int N = 100;
+const int N = 100;							// max number of figures and labels
+
+
 
 int main() {
-	readFile();
+	string path_in;
+	cout << "input file path: ";
+	getline(cin, path_in);
+	changeFiles(path_in);
 	return 0;
 }
 
@@ -33,11 +66,27 @@ int main() {
 //   2) let users choose between labeling with numbers or letters
 //   3) use .doc files. will this read text boxes/other special formatting?
 
-void readFile() {
+void changeFiles(string path_in) {
+
 	
-	ifstream ifile("linked labels test.txt");
-	ofstream ofile("relabled linked labels test.txt");
-	
+	// get old file name
+	char* cpath_in = new char [path_in.length()+1];
+	strcpy(cpath_in, path_in.c_str());
+	ifstream ifile(cpath_in);
+	if (!ifile) {
+		cout << "404 File Not Found \n";
+		return;
+	}
+	delete[] cpath_in;
+
+
+	// create new file name "* RELABELED.xxx"
+	string path_out = newFileName(path_in);
+	char* cpath_out = new char[path_out.length()+1];
+	strcpy(cpath_out, path_out.c_str());
+	ofstream ofile(cpath_out);
+	delete[] cpath_in;
+
 	string str;
 
 	int oldFigLabels[N];
@@ -64,7 +113,7 @@ void readFile() {
 			ofile << newLabel;
 
 		} else if (isApp(str, ifile)) {
-			// replace oldlabel with newlabel
+			// replace oldlabel with newlabel, store oldlabel
 			ifile >> oldAppLabel;
 			newLabel = searchNStoreOldLabels(oldAppLabels, oldAppLabel, appcount) + 1;
 			ofile << newLabel;
@@ -116,16 +165,10 @@ bool isApp(const string& str, ifstream& infile) {
 
 //////////////////////////////////////////////////////////////////////////////////////////////////
 // string compare functions
-// should comparisons be case in/sensitive?
 
 // case insensitive
 bool ciCompareChar(char a, char b) {
 	return tolower(a) == tolower(b);
-}
-
-// case sensitive
-bool csCompareChar(char a, char b) {
-	return a == b;
 }
 
 bool compareStr(const string& str1, const string& str2) {
@@ -143,7 +186,7 @@ bool compareStr(const string& str1, const string& str2) {
 
 
 //////////////////////////////////////////////////////////////////////////////////////////////////
-// get whitespaces. take care of eof
+// get whitespaces
 
 void writeWhiteSpaces(ifstream& infile, ofstream& outfile) {
 
@@ -200,4 +243,23 @@ int searchNStoreOldLabels(int oldLabels[], int oldLabel, int& numLabels) {
 }
 
 // end of search and store function
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+// make name for output file
+// split path_in to name + extension
+// note: most file extensions are three characters long but they don't have to be
+
+string newFileName(string path_in) {
+	string sub1, sub2;
+	string path_out_suf = " RELABELED";
+	int split_pos = path_in.rfind('.');
+	sub1 = path_in.substr(0, split_pos);
+	sub2 = path_in.substr(split_pos, path_in.length());
+	return sub1+path_out_suf+sub2;
+}
+
+// end of newFileName generator
 //////////////////////////////////////////////////////////////////////////////////////////////////
